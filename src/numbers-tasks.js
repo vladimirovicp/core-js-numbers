f5d58f567eca51c8baa4fb27e24c3cc60a2ce70b
vladimirovicp <vladimirovicp70@gmail.com>
2023-11-16 22:35:19 +0400

    add: 37/19

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -337,6 +337,7 @@ function isPowerOfTwo(num) {
 
 /**
  * Returns the sine of a number.
+ * Возвращает синус числа
  *
  * @param {number} num
  * @return {number}
@@ -345,12 +346,13 @@ function isPowerOfTwo(num) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
  * Returns a string representation of a number in a specified base (radix).
+ * Возвращает строковое представление числа в указанной базе (основании).
  *
  * @param {number} number
  * @param {number} base
@@ -360,12 +362,13 @@ function getSine(/* num */) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 
 /**
  * Returns a string representation of a number in exponential notation.
+ * Возвращает строковое представление числа в экспоненциальной системе счисления.
  *
  * @param {number} number
  * @param {number} fractionDigits
@@ -374,12 +377,13 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
 
 /**
  * Returns a string representation of a number in fixed-point notation.
+ * Возвращает строковое представление числа в системе счисления с фиксированной запятой
  *
  * @param {number} number
  * @param {number} fractionDigits
@@ -389,13 +393,15 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits);
 }
 
 /**
  * Returns a string representation of a number in normal (fixed-point or exponential)
  * notation rounded to precision significant digits.
+ * Возвращает строковое представление числа в обычной системе счисления
+ * (с фиксированной точкой или экспоненциальной), округленное до точных значащих цифр.
  *
  * @param {number} number
  * @param {number} precision
@@ -405,12 +411,13 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
  * Returns the primitive value of a Number object.
+ * Возвращает примитивное значение числового объекта
  *
  * @param {Number} number
  * @return {number}
@@ -419,12 +426,13 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
  * Returns a boolean value indicating whether the parameter is a number or not.
+ * Возвращает логическое значение, указывающее, является ли параметр числом или нет.
  *
  * @param {number} number
  * @return {boolean}
@@ -438,12 +446,13 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return Number.isFinite(number);
 }
 
 /**
  * Returns a boolean value indicating whether a number is an integer or not.
+ * Возвращает логическое значение, указывающее, является ли число целым или нет.
  *
  * @param {number} number
  * @return {boolean}
@@ -453,12 +462,14 @@ function isNumber(/* number */) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
  * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN.
+ * Возвращает число с плавающей запятой или, если число не может
+ * быть проанализировано из аргумента, возвращает NaN.
  *
  * @param {string} str
  * @return {number | NaN}
@@ -467,8 +478,8 @@ function isInteger(/* number */) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
