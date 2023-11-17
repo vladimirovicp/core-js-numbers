11c14e572d39832c363eed27e64a12b8e62963e0
vladimirovicp <vladimirovicp70@gmail.com>
2023-11-17 21:30:35 +0400

    feat: serch error

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -576,8 +576,9 @@ function roundToNearestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(number) {
-  return Math.trunc(number);
+function getIntegerPartNumber(/* number */) {
+  throw new Error('Not implemented');
+  /* return Math.trunc(number); */
 }
 
 /**
@@ -593,8 +594,9 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(x1, x2, x3) {
-  return parseFloat((x1 + x2 + x3).toFixed(10));
+function getSumOfNumbers(/* x1, x2, x3 */) {
+  throw new Error('Not implemented');
+  /* return parseFloat((x1 + x2 + x3).toFixed(10)); */
 }
 
 /**
@@ -610,8 +612,9 @@ function getSumOfNumbers(x1, x2, x3) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(firstNumber, secondNumber) {
-  return Math.max(firstNumber, secondNumber);
+function getMaxNumber(/* firstNumber, secondNumber */) {
+  throw new Error('Not implemented');
+  /* return Math.max(firstNumber, secondNumber); */
 }
 
 /**
@@ -627,8 +630,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomInteger(/* min, max */) {
+  /* return Math.random() * (max - min - 1) + min; */
+  throw new Error('Not implemented');
 }
 
 /**
@@ -642,8 +646,9 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(a, b) {
-  return Math.hypot(a, b);
+function getHypotenuse(/* a, b */) {
+  /* return Math.hypot(a, b); */
+  throw new Error('Not implemented');
 }
 
 /**
